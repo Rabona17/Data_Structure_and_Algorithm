46f1e45bf1382e28dd864c50b5b9a05315266eac
Yuan Gao <y1gao@ucsd.edu>
2019-03-22 22:06:59 -0700

    Update MiscTreeFunc.py

--- Tree_Python/MiscTreeFunc.py
+++ Tree_Python/MiscTreeFunc.py
@@ -20,3 +20,19 @@ def sum_labels(t):
     """Sum the labels of a Tree instance, which may be None."""
     return t.label + sum([sum_labels(b) for b in t.branches])
     
+def virtice_match(tree, number):
+    """
+    Given a tree, and a value k, vertice_match() returns the number of vertices in the tree.
+    """
+    count = 0
+    if tree.is_leaf():
+        if tree.label == number:
+            count+=1
+        return count
+    else:
+        if tree.label == number:
+            count+=1
+        for branch in tree.branches:
+            count += virtice_match(branch, number)
+        return count
+    
