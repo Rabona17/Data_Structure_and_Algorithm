29d75cb462266d1e9de0171f0e7f9088b7bbc8b6
Yuan Gao <y1gao@ucsd.edu>
2019-03-27 23:14:07 -0700

    Update BST_Algorithms.py

--- Tree_Python/BST_Algorithms.py
+++ Tree_Python/BST_Algorithms.py
@@ -1,3 +1,7 @@
+##################################################
+##The Following algorithms are done by recursion##
+##Implementation by stack and queues are coming ##
+##################################################
 def PreOrder(bst):
   """
   Return the PreOrder Traversal of a bst
@@ -13,3 +17,33 @@ def PreOrder(bst):
         traversal.extend(PreOrder(bst.left))        
         traversal.extend(PreOrder(bst.right))
         return traversal
+
+def InOrder(bst):
+  """
+  Return the InOrder Traversal of a BST
+  """
+    traversal=[]
+    if bst == BSTree.empty:
+        return traversal
+    elif bst.is_leaf():
+        traversal.extend([bst.label])
+        return traversal
+    else:
+        traversal.extend(InOrder(bst.left))  
+        traversal.extend([bst.label])              
+        traversal.extend(InOrder(bst.right))
+        return traversal
+
+def PostOrder(bst):
+    traversal=[]
+    if bst == BSTree.empty:
+        return traversal
+    elif bst.is_leaf():
+        traversal.extend([bst.label])
+        return traversal
+    else:
+        traversal.extend(PostOrder(bst.left))    
+        traversal.extend(PostOrder(bst.right))
+        traversal.extend([bst.label])            
+        return traversal
+      
