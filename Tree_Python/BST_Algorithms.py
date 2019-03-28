245e11e0feed7db5ee7d9bc8a08ab697a2b41570
Yuan Gao <y1gao@ucsd.edu>
2019-03-27 23:08:03 -0700

    Update BST_Algorithms.py

--- Tree_Python/BST_Algorithms.py
+++ Tree_Python/BST_Algorithms.py
@@ -1,1 +1,15 @@
-
+def PreOrder(bst):
+  """
+  Return the PreOrder Traversal of a bst
+  """
+    traversal=[]
+    if bst == BSTree.empty:
+        return traversal
+    elif bst.is_leaf():
+        traversal.extend([bst.label])
+        return traversal
+    else:
+        traversal.extend([bst.label])
+        traversal.extend(PreOrder(bst.left))        
+        traversal.extend(PreOrder(bst.right))
+        return traversal
